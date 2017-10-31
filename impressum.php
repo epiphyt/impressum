f66d7c7e7abb2db692e6c503dfaebebf9e9e45d4
Simon Kraft <Simon@krafit.de>
2017-10-31 13:03:38 +0100

    :tada: Initial commit

---
+++ impressum.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name:  Impressum
+Plugin URI:   https://impressum.plus
+Description:  Simple Impressum Generator
+Version:      0.1
+Author:       Matthias Kittsteiner, Simon Kraft
+License:      GPL3
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  impressum
+Domain Path:  /languages
+
+
+Impressum is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+ 
+Impressum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Impressum. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+*/

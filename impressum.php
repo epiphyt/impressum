4d9914dd901f5f7fa804b3801177e8a66877ae88
Simon Kraft <simon@krafit.de>
2018-05-24 17:52:48 +0200

    :bookmark: Version bump
    closes #37

--- impressum.php
+++ impressum.php
@@ -3,7 +3,7 @@
 Plugin Name:	Impressum
 Plugin URI:		https://wordpress.org/plugins/impressum/
 Description:	Simple Impressum Generator
-Version:		0.1
+Version:		0.2
 Author:			Epiphyt
 Author URI:		https://epiph.yt/
 License:		GPL3

1a2344a9daa26527eb504a4c2ce04bcd12c87a54
Matthias Kittsteiner <mk@kittmedia.com>
2018-10-13 12:02:49 +0200

    :bug: Fix check for VAT ID if there is no one set

--- inc/class-impressum-frontend.php
+++ inc/class-impressum-frontend.php
@@ -678,7 +678,10 @@ class Impressum_Frontend extends Impressum {
 		}
 		
 		// special case for VAT
-		if ( ! isset( $invalid_fields['vat_id'] ) ) {
+		if (
+			! \in_array( $options['legal_entity'], [ 'individual', 'self' ], true )
+			&& ! isset( $invalid_fields['vat_id'] )
+		) {
 			$regex = '/^((AT)?U[0-9]{8}|(BE)?0[0-9]{9}|(BG)?[0-9]{9,10}|(CY)?[0-9]{8}L|(CZ)?[0-9]{8,10}|(DE)?[0-9]{9}|(DK)?[0-9]{8}|(EE)?[0-9]{9}|(EL|GR)?[0-9]{9}|(ES)?[0-9A-Z][0-9]{7}[0-9A-Z]|(FI)?[0-9]{8}|(FR)?[0-9A-Z]{2}[0-9]{9}|(GB)?([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|(HU)?[0-9]{8}|(IE)?[0-9]S[0-9]{5}L|(IT)?[0-9]{11}|(LT)?([0-9]{9}|[0-9]{12})|(LU)?[0-9]{8}|(LV)?[0-9]{11}|(MT)?[0-9]{8}|(NL)?[0-9]{9}B[0-9]{2}|(PL)?[0-9]{10}|(PT)?[0-9]{9}|(RO)?[0-9]{2,10}|(SE)?[0-9]{12}|(SI)?[0-9]{8}|(SK)?[0-9]{10})$/';
 			
 			if ( ! preg_match( $regex, $options['vat_id'] ) ) {

e5b21537ae0c2836e2983bb97e23900f9129ba15
Matthias Kittsteiner <mk@kittmedia.com>
2025-05-20 10:05:35 +0200

    🐛 Fix check for VAT ID and Business ID

--- inc/class-admin.php
+++ inc/class-admin.php
@@ -204,19 +204,22 @@ class Admin {
 				Impressum::get_instance()->settings_fields['contact_form_page']['title']
 			);
 		}
-		// special case for VAT and business ID
-		else if ( ! isset( $invalid_fields['vat_id'] ) ) {
+		
+		// special case for VAT ID
+		if ( ! isset( $invalid_fields['vat_id'] ) ) {
 			$regex = '/^(|ATU[0-9]{8}|BE0[0-9]{9}|BG[0-9]{9,10}|CY[0-9]{8}L|CZ[0-9]{8,10}|DE[0-9]{9}|DK[0-9]{8}|EE[0-9]{9}|(EL|GR)[0-9]{9}|ES[0-9A-Z][0-9]{7}[0-9A-Z]|FI[0-9]{8}|FR[0-9A-Z]{2}[0-9]{9}|GB([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|HU[0-9]{8}|IE[0-9]S[0-9]{5}L|IT[0-9]{11}|LT([0-9]{9}|[0-9]{12})|LU[0-9]{8}|LV[0-9]{11}|MT[0-9]{8}|NL[0-9\+\*]{9}B[0-9]{2}|PL[0-9]{10}|PT[0-9]{9}|RO[0-9]{2,10}|SE[0-9]{12}|SI[0-9]{8}|SK[0-9]{10})$/';
 			
 			if ( ! empty( $options['vat_id'] ) && ! \preg_match( $regex, $options['vat_id'] ) ) {
 				$invalid_fields['vat_id'] = Impressum::get_instance()->settings_fields['vat_id']['title'];
 			}
 		}
-		else if ( ! isset( $invalid_fields['business_id'] ) ) {
+		
+		// special case for business ID
+		if ( ! isset( $invalid_fields['business_id'] ) ) {
 			$regex = '/^(|(DE)?[0-9]{9}\-[0-9]{5})$/';
 			
 			if ( ! empty( $options['business_id'] ) && ! \preg_match( $regex, $options['business_id'] ) ) {
-				$invalid_fields['business_id'] = Impressum::get_instance()->settings_fields['vat_id']['title'];
+				$invalid_fields['business_id'] = Impressum::get_instance()->settings_fields['business_id']['title'];
 			}
 		}
 		

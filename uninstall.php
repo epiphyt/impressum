7ed10b0263a963e69f8d30791c2c1f1d4afcfb3e
Matthias Kittsteiner <mk@kittmedia.com>
2026-07-26 13:20:28 +0200

    🧐 Clean up all plugin data and cron on uninstall

--- uninstall.php
+++ uninstall.php
@@ -12,7 +12,9 @@ if ( \is_plugin_active( 'impressum-plus/impressum-plus.php' ) ) {
 }
 
 $options = [
+	'dismissed-impressum_validation_notice',
 	'dismissed-impressum_welcome_notice',
+	'impressum_field_data',
 	'impressum_imprint_options',
 ];
 
@@ -21,3 +23,6 @@ foreach ( $options as $option ) {
 	// for site options in multisite
 	\delete_site_option( $option );
 }
+
+// remove the scheduled cron event
+\wp_clear_scheduled_hook( 'impressum_twice_daily_cron' );
